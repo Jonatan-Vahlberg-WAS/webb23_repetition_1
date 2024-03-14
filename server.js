a3ccb04ebd9279ea99788f5e52ef22b030578496
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-14 10:44:00 +0100

    cleanup

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = require("./app/app");
 // use the PORT environment variable if it exists
 const port = process.env.PORT || 3000;
 
-console.log("ENV; ",process.env.PORT);
-
+// start the server and listen on the port
 app.listen(port, () => {
   console.log("Server is running on port" + port);
 })
