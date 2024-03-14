b5d9d2349d18103c3d6757ad7b2745755d1a6d0d
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-03-14 15:29:14 +0100

    add requests post/ get

--- app/routes/authors.js
+++ app/routes/authors.js
@@ -1,30 +1,60 @@
 const { Router } = require("express");
-const { readDatabaseFile } = require("../utils/database");
+const { readDatabaseFile, generateUniqueId, writeDatabaseFile } = require("../utils/database");
 
-const authorDatabasePath = "./app/data/authors.json"
+const authorDatabasePath = "./app/data/authors.json";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-    try {
-        const authors = await readDatabaseFile(authorDatabasePath);
-        res.json(authors)
-    } catch (error) {
-        console.log("error: getting authors", error.message)
-        res.status(500).json({
-            message: error.message
-        })
+  try {
+    // get authors from database
+    const authors = (await readDatabaseFile(authorDatabasePath)) || [];
+    res.json(authors);
+  } catch (error) {
+    console.log("error: getting authors", error.message);
+    // Should be inacessible
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const authors = (await readDatabaseFile(authorDatabasePath)) || [];
+    const author = authors.find((author) => author.id == id);
+    if (!author) {
+      return res.status(404).json({
+        message: "Author not found",
+      });
     }
+    res.json(author);
+  } catch (error) {
+    console.log("error: getting authors", error.message);
+    // Should be inacessible
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-router.get("/:id", (req, res) => {
-  //TODO: add get author logic here
-  res.status(404).send("Not implemented");
-});
-
-router.post("/", (req, res) => {
-  //TODO: add create author logic here
-  res.status(404).send("Not implemented");
+router.post("/", async (req, res) => {
+    try {
+        let newAuthor = req.body;
+        newAuthor.id = generateUniqueId()
+        //TODO: validation
+        let authors = await readDatabaseFile(authorDatabasePath) || [];
+        authors.push(newAuthor)
+        await writeDatabaseFile(authors)
+        res.json(newAuthor);
+  } catch (error) {
+    console.log("error: creating author", error.message);
+    // Should be inacessible
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
 router.put("/:id", (req, res) => {
